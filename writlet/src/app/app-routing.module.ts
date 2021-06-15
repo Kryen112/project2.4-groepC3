8962c1b8bfcad0de6ca132bc1fb551aa29c11114
Kryen112 <Kryen112@hotmail.com>
2021-06-15 11:18:35 +0200

    temp

--- writlet/src/app/app-routing.module.ts
+++ writlet/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import {ContactComponent} from "./contact/contact.component";
 import {HelpComponent} from "./help/help.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent},
   { path: 'termsconditions', component: TermsconditionsComponent},
